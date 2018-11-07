46853f465564d51e7e1883ec77ff2b17664d56b3
kimseulbi <ksb5166@gmail.com>
2018-11-07 16:20:50 +0900

    로그인화면 디자인 및 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import '@babel/polyfill' // 이 라인을 지우지 말아주세요!
+import "@babel/polyfill"; // 이 라인을 지우지 말아주세요!
 
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.API_URL
-})
+});
 
-api.interceptors.request.use(function (config) {
-  const token = localStorage.getItem('token')
+api.interceptors.request.use(function(config) {
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers || {}
-    config.headers['Authorization'] = 'Bearer ' + token
+    config.headers = config.headers || {};
+    config.headers["Authorization"] = "Bearer " + token;
   }
-  return config
+  return config;
 });
 
 const templates = {
+  loginForm: document.querySelector("#login").content
+};
 
-}
-
-const rootEl = document.querySelector('.root')
+const rootEl = document.querySelector(".container");
 
 // 페이지 그리는 함수 작성 순서
 // 1. 템플릿 복사
@@ -28,3 +28,39 @@ const rootEl = document.querySelector('.root')
 // 4. 내용 채우기
 // 5. 이벤트 리스너 등록하기
 // 6. 템플릿을 문서에 삽입
+
+async function drawLoginForm() {
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.loginForm, true);
+  // 2. 요소 선택
+  const formEl = frag.querySelector(".login-form");
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener("submit", async e => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+
+    const res = await api.post("/users/login", {
+      username,
+      password
+    });
+    localStorage.setItem("token", res.data.token);
+      contentPage();
+  });
+
+  // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = "";
+  rootEl.appendChild(frag);
+}
+
+// 메인페이지
+async function contentPage() {}
+
+// 페이지 로드 시 그릴 화면 설정
+if (localStorage.getItem("token")) {
+  contentPage();
+} else {
+  drawLoginForm();
+}
